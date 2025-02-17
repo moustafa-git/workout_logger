7dfb0ccfdf76e520bb771cfb45b8a61753666e9e
moustafa-git <mostax266@gmail.com>
2025-02-18 01:50:10 +0200

    onclick popup

--- script.js
+++ script.js
@@ -22,20 +22,37 @@ if (navigator.geolocation) {
 
       console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
-      var map = L.map('map').setView(coord, 13);
+      const map = L.map('map').setView(coord, 13);
 
       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coord)
-        .addTo(map)
-        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-        .openPopup();
+      map.on('click', function (mapEvent) {
+        const { lat, lng } = mapEvent.latlng;
+
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('Workout')
+          .openPopup();
+      });
     },
     function () {
       alert('Could not get your current position.');
     }
   );
+
+  // map.on('click', function (mapEvent) {
+  //   console.log(mapEvent);
+  // });
 }
